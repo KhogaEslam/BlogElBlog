0327dc876349579e3a7a0eaf7ecad63be0b1767b
Eslam Gamal Khoga <KhogaEslam@Gmail.com>
2017-02-06 18:42:37 +0200

    Delete admin.py

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import post, category, comment, word_list
-admin.site.register(post)
-admin.site.register(category)
-admin.site.register(comment)
-admin.site.register(word_list)
