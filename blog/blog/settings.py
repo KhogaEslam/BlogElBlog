ed1d1b72560908da7e4e707d3a288305a0781254
anas-marrey <anas.a.marrey@outlook.com>
2017-02-06 11:22:47 +0200

    body of home pages

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'root'
     }
 }
 
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
