4bbb13fdb5cb7c0048d4d8c9d5675f1fb537ada8
salmaGaber <salmaalex200@gmail.com>
2017-02-08 19:13:24 +0200

    fixing missed code

--- blog/adminpanel/forms.py
+++ blog/adminpanel/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from main.models import category
+from django import forms
 
 class RegisterForm(UserCreationForm):
     class Meta:
@@ -9,3 +11,9 @@ class RegisterForm(UserCreationForm):
         super(RegisterForm, self).__init__(*args, **kwargs)
         self.fields["username"].label = "User Name"
         self.fields["email"].label = "Email Address"
+
+
+class category_form(forms.ModelForm):
+    class Meta:
+        model = category
+        fields = ['cat_title',]        
